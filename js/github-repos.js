49f2f01b164a927712264c5097932dd61e5cac40
Terry <b.terry.thompson@gmail.com>
2017-03-06 12:08:25 -0500

    updated auto table population for repos.html

--- js/github-repos.js
+++ js/github-repos.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-      $.getJSON('https://api.github.com/users/terrythompson/repos', function(reposResults) {
-          console.log(reposResults);
+      $.getJSON('https://api.github.com/users/terrythompson/repos', function(response) {
+
+         for (var repoIndex = 0; repoIndex < response.length; repoIndex++) {
+           var repo = response[repoIndex];
+           var repoName = repo.name;
+           var repoUpdated = repo.updated_at;
+           var repoLink = repo.html_url;
+
+           $('#reposTableBody').append("<tr><td class=\"mdl-data-table__cell--non-numeric\">" + repoName +
+           "</td><td>" + repoUpdated + "</td><td class=\"mdl-data-table__cell--non-numeric\">"
+           + repoLink + "</td></tr>");
+         }
+
       });
 });
